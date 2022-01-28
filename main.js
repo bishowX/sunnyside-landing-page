86041f37c96de834cad767630857f16c0d2e66e5
bishowX <bishowpandu@gmail.com>
2022-01-28 21:43:00 +0545

    final build

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-import 'style.css'
+import './style.css'
+
+const menuBtn = document.getElementById('menu-btn')
+const mobileMenuOverlay = document.getElementById('overlay')
+
+
+const closeMenu = () => {
+	mobileMenuOverlay.classList.remove('block')
+	mobileMenuOverlay.classList.add('hidden')
+}
+
+
+menuBtn.addEventListener('click', event => {
+	let active = mobileMenuOverlay.classList.contains('block')
+	if (active) closeMenu()
+	else {
+		mobileMenuOverlay.classList.remove('hidden')
+		mobileMenuOverlay.classList.add('block')
+	}
+})
+
+
+mobileMenuOverlay.addEventListener('click', event => {
+	event.stopPropagation()
+	closeMenu()
+})
